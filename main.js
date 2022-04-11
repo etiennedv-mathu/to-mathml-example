81ce646c8df4256d020f4c951c883f0bfc3b8f01
Etienne de Villiers <etienne@mathu.co.za>
2022-04-11 09:25:47 +0200

    removed comment

--- main.js
+++ main.js
@@ -20,14 +20,4 @@ mjAPI.typeset({
   mml:true,      // or svg:true, or html:true
 }, function (data) {
   if (!data.errors) {console.log(data.mml)}
-  // will produce:
-  // <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
-  //   <mi>E</mi>
-  //   <mo>=</mo>
-  //   <mi>m</mi>
-  //   <msup>
-  //     <mi>c</mi>
-  //     <mn>2</mn>
-  //   </msup>
-  // </math>
 });
